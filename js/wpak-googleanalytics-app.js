5ba6b6afe223aa7d079828587329f94b62cbda67
Lionel Pointet <lionel.pointet@virtua.ch>
2016-02-01 17:35:33 +0100

    Add some event tracking: all info events, all error events, app-version-changed and refresh

--- js/wpak-googleanalytics-app.js
+++ js/wpak-googleanalytics-app.js
@@ -3,6 +3,29 @@ define( [ 'core/theme-app', 'addons/wpak-addon-googleanalytics/js/wpak-googleana
 
     App.on( 'screen:showed', function( current_screen, current_view ) {
         // WpakGoogleAnalytics.tracker.trackView( 'Screen Title' )
-        // WpakGoogleAnalytics.tracker.trackEvent( 'Category', 'Action', 'Label', Value )
+    });
+
+    App.on( 'error', function( error ) {
+        var message = '';
+
+        if( typeof error.data != "undefined" && typeof error.data.message != "undefined" ) {
+            message = error.data.message;
+        }
+
+        WpakGoogleAnalytics.tracker.trackEvent( 'app', error.event, message );
+    });
+
+    App.on( 'info', function( info ) {
+        var message = '';
+
+        if( info.event == 'app-version-changed' ) {
+            message = info.data.stats.version_diff.last_version + ' => ' + info.data.stats.version_diff.current_version;
+        }
+
+        WpakGoogleAnalytics.tracker.trackEvent( 'app', info.event, message );
+    });
+
+    App.on( 'refresh:start', function() {
+        WpakGoogleAnalytics.tracker.trackEvent( 'app', 'refresh' );
     });
 });
