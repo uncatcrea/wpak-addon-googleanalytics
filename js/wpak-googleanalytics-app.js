116fb03841ef0987b5746e14e7c55403ba7955aa
Lionel Pointet <lionel.pointet@virtua.ch>
2016-02-02 17:53:12 +0100

    Add some comments, avoid sending 'no-content' info event

--- js/wpak-googleanalytics-app.js
+++ js/wpak-googleanalytics-app.js
@@ -2,13 +2,20 @@ define( [ 'core/theme-app', 'addons/wpak-addon-googleanalytics/js/wpak-googleana
     var track = WpakGoogleAnalytics.init();
 
     if( track ) {
+        /**
+         * Track page views
+         */
         App.on( 'screen:showed', function( current_screen, current_view ) {
             // WpakGoogleAnalytics.tracker.trackView( 'Screen Title' )
         });
 
+        /**
+         * Track error events
+         */
         App.on( 'error', function( error ) {
             var message = '';
 
+            // Include the error message as event label if it's available
             if( typeof error.data != "undefined" && typeof error.data.message != "undefined" ) {
                 message = error.data.message;
             }
@@ -16,16 +23,28 @@ define( [ 'core/theme-app', 'addons/wpak-addon-googleanalytics/js/wpak-googleana
             WpakGoogleAnalytics.tracker.trackEvent( 'app', error.event, message );
         });
 
+        /**
+         * Track info events
+         */
         App.on( 'info', function( info ) {
             var message = '';
 
+            // If app version changed, include both old and new version into the event label
             if( info.event == 'app-version-changed' ) {
                 message = info.data.stats.version_diff.last_version + ' => ' + info.data.stats.version_diff.current_version;
+            }
+
+            // Do nothing for 'no-content' info event, as an error should have been fired first and this one is unrelevant for Google Analytics
+            if( info.event == 'no-content' ) {
+                return;
             }
 
             WpakGoogleAnalytics.tracker.trackEvent( 'app', info.event, message );
         });
 
+        /**
+         * Track content refresh
+         */
         App.on( 'refresh:start', function() {
             WpakGoogleAnalytics.tracker.trackEvent( 'app', 'refresh' );
         });
--- js/wpak-googleanalytics.js
+++ js/wpak-googleanalytics.js
@@ -9,11 +9,19 @@ define( function( require ) {
         tracker: null
     };
 
+    /**
+     * Initialize Google Analytics plugin:
+     *  - Expose tracker object to be used within plugins and themes
+     *  - Activate tracking thanks to the given tracking ID
+     *
+     * @returns {Boolean} True when everything went fine, false otherwise
+     */
     googleanalytics.init = function() {
         if( !Phonegap.isLoaded() || "undefined" == typeof Config.options.googleanalytics || "undefined" == typeof window.analytics ) {
             return false;
         }
 
+        // Expose Google Analytics plugin object for themes and plugins to use its methods, especially trackView and trackEvent
         googleanalytics.tracker = window.analytics;
 
         // Activate debug mode
