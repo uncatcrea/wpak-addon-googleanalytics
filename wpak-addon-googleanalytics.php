cf6de0cc036f2e778083afbb1c4b8f753dbb6285
Lionel Pointet <lionel.pointet@gmail.com>
2017-09-25 20:24:01 +0200

    Upgrade cordova plugin

--- wpak-addon-googleanalytics.php
+++ wpak-addon-googleanalytics.php
@@ -64,7 +64,7 @@ if ( !class_exists( 'WpAppKitGoogleAnalytics' ) ) {
          */
         public static function wpak_default_phonegap_build_plugins( $default_plugins, $export_type, $app_id ) {
             if( WpakAddons::addon_activated_for_app( self::slug, $app_id ) ) {
-                $default_plugins['cordova-plugin-google-analytics'] = array( 'spec' => '1.8.2', 'source' => 'npm' );
+                $default_plugins['cordova-plugin-google-analytics'] = array( 'spec' => '1.8.3', 'source' => 'npm' );
             }
 
             return $default_plugins;
